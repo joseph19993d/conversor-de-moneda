5429a5e2c17312dabcdee92855a6824380733f24
joseph19993d <rodelosuaresj@gmail.com>
2024-10-11 03:24:21 -0500

    Agregando comentarios

--- src/co/com/conversor/moneda/model/ConvertidorMoneda.java
+++ src/co/com/conversor/moneda/model/ConvertidorMoneda.java
@@ -10,40 +10,50 @@ import java.net.http.HttpResponse;
 
 public class ConvertidorMoneda {
 
-
+    // Variables para almacenar las monedas seleccionadas
     private String moneda = "";
     private String otraMoneda = "";
 
+    // Método para obtener la otra moneda
     public String getOtraMoneda() {
         return otraMoneda;
     }
 
+    // Método para establecer la otra moneda
     public void setOtraMoneda(String otraMoneda) {
         this.otraMoneda = otraMoneda;
     }
 
+    // Constructor por defecto
     public ConvertidorMoneda() {
     }
 
+    // Constructor con parámetros
     public ConvertidorMoneda(String moneda, String otraMoneda) {
         this.moneda = moneda;
         this.otraMoneda = otraMoneda;
     }
 
+    // Método para obtener la moneda
     public String getMoneda() {
         return moneda;
     }
 
+    // Método para establecer la moneda
     public void setMoneda(String moneda) {
         this.moneda = moneda;
     }
 
+    // Cliente HTTP para realizar solicitudes
     HttpClient client = HttpClient.newHttpClient();
 
-    public double convertirMoneda (int option){
+    // Método para convertir moneda según la opción seleccionada
+    public double convertirMoneda(int option) {
 
-        System.out.println("Opcion elegida : " + option);
+        // Imprime la opción elegida por el usuario
+        System.out.println("Opción elegida: " + option);
 
+        // Configura las monedas según la opción seleccionada
         switch (option) {
             case 1:
                 setMoneda("USD");
@@ -69,42 +79,47 @@ public class ConvertidorMoneda {
                 setMoneda("COP");
                 setOtraMoneda("USD");
                 break;
-
             default:
-                System.out.println("hi");
-
+                System.out.println("Opción no válida.");
+                return 0; // Retorna 0 si la opción no es válida
         }
 
-        URI moneda = URI.create("https://v6.exchangerate-api.com/v6/33b400a67d5b1ba70584ecb4/latest/"+this.moneda);
+        // Crea la URI para la solicitud HTTP utilizando la moneda seleccionada
+        URI moneda = URI.create("https://v6.exchangerate-api.com/v6/33b400a67d5b1ba70584ecb4/latest/" + this.moneda);
         HttpRequest request = HttpRequest.newBuilder().uri(moneda).build();
         HttpResponse<String> response = null;
 
         try {
+            // Envía la solicitud y obtiene la respuesta
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (Exception e) {
             throw new RuntimeException("Error en la solicitud HTTP.");
         }
 
+        // Crea una instancia de Gson para manejar JSON
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
         try {
             // Convierte el cuerpo de la respuesta JSON a un objeto MonedaValue
             MonedaValue monedaValue = gson.fromJson(response.body(), MonedaValue.class);
-            // Accede a la tasa de cambio de la moneda
-            Double Rate = monedaValue.conversion_rates().get(this.otraMoneda);
 
-            if (Rate != null) {
-                System.out.println("El precio de la moneda ("+this.moneda+") en moneda ("+this.otraMoneda+") es: " + Rate);
-                return Rate;
+            // Accede a la tasa de cambio de la otra moneda
+            Double rate = monedaValue.conversion_rates().get(this.otraMoneda);
+
+            if (rate != null) {
+                // Imprime y retorna la tasa de cambio encontrada
+                System.out.println("El precio de la moneda (" + this.moneda + ") en moneda (" + this.otraMoneda + ") es: " + rate);
+                return rate;
             } else {
-                System.out.println("No se encontró la tasa de ("+this.moneda+") a ("+ this.otraMoneda+ ")");
+                // Imprime un mensaje si no se encuentra la tasa de cambio
+                System.out.println("No se encontró la tasa de (" + this.moneda + ") a (" + this.otraMoneda + ")");
                 return 0;
             }
 
         } catch (Exception e) {
+            // Maneja excepciones relacionadas con la conversión de JSON
             System.err.println("Error al convertir el JSON a un objeto MonedaValue: " + e.getMessage());
             return 0;
         }
-
     }
 }
--- src/co/com/conversor/moneda/principal/Main.java
+++ src/co/com/conversor/moneda/principal/Main.java
@@ -7,39 +7,52 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Bucle infinito para que la aplicación siga ejecutándose hasta que el usuario decida salir
         while (true) {
+            // Instancia de la clase ConvertidorMoneda
             ConvertidorMoneda convertir = new ConvertidorMoneda();
+            // Scanner para leer la entrada del usuario
             Scanner scanner = new Scanner(System.in);
+
+            // Muestra las opciones de conversión disponibles al usuario
             System.out.println(
-                """
-                    *******************************************
-                    1. **Dólar (USD) => Peso Argentino (ARS)
-                    2. **Peso Argentino (ARS) => Dólar (USD)
-                    3. **Dólar (USD) => Real Brasileño (BRL)
-                    4. **Real Brasileño (BRL) => Dólar (USD)
-                    5. **Dólar (USD) => Peso Colombiano (COP)
-                    6. **Peso Colombiano (COP) => Dólar (USD)
-                    7. **Salir**: Cierra la aplicación.
-                    *******************************************
-                
-                    *-->Ingrese la opcion de su preferencia<--*
-                
-                    *******************************************
-                """
+                    """
+                        *******************************************
+                        1. **Dólar (USD) => Peso Argentino (ARS)
+                        2. **Peso Argentino (ARS) => Dólar (USD)
+                        3. **Dólar (USD) => Real Brasileño (BRL)
+                        4. **Real Brasileño (BRL) => Dólar (USD)
+                        5. **Dólar (USD) => Peso Colombiano (COP)
+                        6. **Peso Colombiano (COP) => Dólar (USD)
+                        7. **Salir**: Cierra la aplicación.
+                        *******************************************
+                    
+                        *-->Ingrese la opción de su preferencia<--*
+                    
+                        *******************************************
+                    """
             );
 
+            // Lee la opción ingresada por el usuario
             int moneda = scanner.nextInt();
 
-            if (moneda == 7) {break;} else if (moneda < 7) {
+            // Si el usuario elige la opción 7, se rompe el bucle y se cierra la aplicación
+            if (moneda == 7) {
+                break;
+            } else if (moneda < 7) {
+                // Llama al método para convertir moneda según la opción elegida
                 double valorDeLaMoneda = convertir.convertirMoneda(moneda);
-                System.out.println("-->¿Cuanto desea convertir? <--");
+
+                // Pide al usuario que ingrese la cantidad que desea convertir
+                System.out.println("-->¿Cuánto desea convertir? <--");
                 double cantidad = scanner.nextDouble();
+
+                // Instancia de la clase CalculardoraMoneda para realizar el cálculo de conversión
                 CalculardoraMoneda calculardoraMoneda = new CalculardoraMoneda(valorDeLaMoneda, cantidad);
+
+                // Muestra el resultado de la conversión al usuario
                 calculardoraMoneda.decirTotal(convertir.getMoneda(), convertir.getOtraMoneda());
             }
-
-
         }
-
     }
 }
